b2670b73d83d8edbbecabb1c4593a4b45f9cff67
mackoo13 <kocot@student.agh.edu.pl>
2019-02-11 20:47:07 +0100

    Pareto, implemented missing rules

--- pmp/rules/__init__.py
+++ pmp/rules/__init__.py
@@ -9,3 +9,5 @@ from .multigoal_rule import MultigoalRule
 from .multigoal_cc_borda import MultigoalCCBorda
 from .multigoal_multibloc import MultigoalTBloc
 from .multigoal_bloc_borda import MultigoalBlocBorda
+from .multigoal_bloc_sntv import MultigoalBlocSNTV
+from .multigoal_borda_sntv import MultigoalBordaSNTV
