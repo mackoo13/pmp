833124fdd6c6b98bcd11523a05145a8a03c58eeb
mackoo13 <kocot@student.agh.edu.pl>
2019-02-11 20:47:07 +0100

    Pareto, implemented missing rules

---
+++ pmp/rules/multigoal_bloc_sntv.py
@@ -0,0 +1,33 @@
+from pmp.rules import SNTV
+from .._common import solve_methods_registry
+from .threshold_rule import ThresholdRule
+from .multigoal_rule import MultigoalRule
+from .bloc import Bloc
+
+algorithm = solve_methods_registry()
+
+
+class MultigoalBlocSNTV(MultigoalRule):
+    methods = algorithm.registry
+
+    def __init__(self, (s1, s2)=(0, 0), weights=None, log_errors=True):
+        MultigoalRule.__init__(self,
+                               [ThresholdRule(Bloc(), s1),
+                                ThresholdRule(SNTV(), s2)],
+                               log_errors=log_errors)
+        self.weights = weights
+
+    def find_committee(self, k, profile, method=None, criterion='max_appr'):
+        if method is None:
+            committee = algorithm.registry.default(self, k, profile, criterion=criterion)
+        else:
+            committee = algorithm.registry.all[method](self, k, profile, criterion=criterion)
+        return committee
+
+    @algorithm('Bruteforce', 'Exponential.')
+    def _brute_bloc_borda(self, k, profile, criterion='max_appr'):
+        return self._brute(k, profile, criterion=criterion)
+
+    @algorithm('ILP', default=True)
+    def _ilp(self, k, profile, criterion='max_appr'):
+        return self._ilp_weakly_separable(k, profile, criterion=criterion)
